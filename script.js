03c693130e5eac183765d21dbb165c40271e6899
keyur <13107634+kyurshah@users.noreply.github.com>
2024-12-23 18:00:51 +0530

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+const display = document.querySelector("#display");
+
+const digitButtons = document.querySelectorAll(".digitButton");
+const operatorButtons = document.querySelectorAll(".operatorButton");
+const equalButton = document.querySelector(".equalButton");
+const clearButton = document.querySelector(".clearButton");
+
+
+
+let num1 = 0;
+let operator = "";
+let num2 = 0;
+
 function add(a,b) {
     return a + b ;
 }
 
 function subtract(a,b) {
-    return a - b ;
+    return b - a ;
 }
 
 function multiply(a,b) {
@@ -11,28 +24,26 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    return a / b ;
+    return b / a ;
 }
 
-let num1 = 0;
-let operator = "";
-let num2 = 0;
+
 
 function operate(operator, num1, num2) {
     switch (operator) {
-        case "add":
+        case "+":
         return add(num1, num2);
         break;
 
-        case "subtract":
+        case "-":
         return subtract(num1, num2);
         break;
 
-        case "multiply":
+        case "*":
         return multiply(num1, num2);
         break;
         
-        case "divide":
+        case "/":
         return divide(num1, num2);
         break;  
         
@@ -41,3 +52,43 @@ function operate(operator, num1, num2) {
     }
 
 }
+
+
+digitButtons.forEach((digitButton) => {
+    digitButton.addEventListener("click", function() {
+
+        const value = digitButton.textContent;
+        display.innerText += value;
+        num1 += value;
+    })
+});
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", function() {
+
+        const operatorValue = operatorButton.textContent;
+        display.innerText += operatorValue;
+        operator = operatorValue;
+        num2 = num1;
+        num1 = "";
+    })
+});
+
+equalButton.addEventListener("click", function() {
+    let result = operate(operator, parseFloat(num1), parseFloat(num2));
+    display.innerText = result;
+    num2 = 0
+    operator = "";
+    num1 = result;
+
+
+})
+
+clearButton.addEventListener("click", function() {
+    display.innerText = "";
+    num2 = ""
+    operator = "";
+    num1 = "";
+    result = ""
+
+})
